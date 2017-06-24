2794a3d46e7475b2ec0fcf04643dce4ab8f691be
张红军 <zhanghongjun228@163.com>
2017-06-24 10:14:21 +0800

    路由不显示

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
     <div>
         <v-header></v-header>
         <div class="tab">
-            <div class="tab-item">商品
-                <router-link to="{path:'/goods'}">商品</router-link>
+            <div class="tab-item">
+                <router-link to="/goods">商品</router-link>
             </div>
             <div class="tab-item">
-                <router-link to="{path:'/ratings'}">评论</router-link>
+                <router-link to="/ratings">评论</router-link>
             </div>
             <div class="tab-item">
-                <router-link to="{path:'/sellers'}">商家</router-link>
+                <router-link to="/sellers">商家</router-link>
             </div>
         </div>
         <router-view></router-view>
@@ -36,5 +36,4 @@
         .tab-item
             flex: 1
             text-align: center
-
 </style>
